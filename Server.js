48c072eec6986cc58e9f75c77e84f5fd6b6c3341
Rehman Khan <rehmankhan69755@gmail.com>
2026-03-08 00:12:59 +0530

    Server.js
    This contains the code for server.js file.

--- Server.js
+++ Server.js
@@ -1,147 +1,66 @@
-const express = require("express")
-const mysql = require("mysql2")
-const cors = require("cors")
-
-const app = express()
-
+const express=require("express")
+const mysql=require("mysql2")
+const cors=require("cors")
+const app=express()
 app.use(cors())
 app.use(express.json())
 
-const db = mysql.createConnection({
- host: "localhost",
- user: "root",
- password: "22ndregiment",
- database: "mythosss"
+const db=mysql.createConnection({
+host:"localhost",
+user:"root",
+password:"22ndregiment",
+database:"mythosss"
 })
 
-db.connect(err => {
- if(err){
-  console.log(err)
- } else{
-  console.log("Connected to MySQL")
- }
-})
+db.connect(err=>{if(err)console.log(err);else console.log("Connected to MySQL")})
 
 app.post("/register",(req,res)=>{
- const {username,password}=req.body
- db.query(
-  "INSERT INTO users (username,password) VALUES (?,?)",
-  [username,password],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send("registered")
-   }
-  }
- )
+const {username,password}=req.body
+db.query("INSERT INTO users(username,password) VALUES(?,?)",[username,password],(err,result)=>{
+if(err)res.send("Error");else res.send("OK")
+})
 })
 
 app.post("/login",(req,res)=>{
- const {username,password}=req.body
- db.query(
-  "SELECT * FROM users WHERE username=? AND password=?",
-  [username,password],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send(result)
-   }
-  }
- )
+const {username,password}=req.body
+db.query("SELECT * FROM users WHERE username=? AND password=?",[username,password],(err,result)=>{
+if(err)res.send([]);else res.json(result)
 })
-
-app.post("/profile",(req,res)=>{
- const {username,name,bio}=req.body
- db.query(
-  "INSERT INTO profiles (username,name,bio) VALUES (?,?,?)",
-  [username,name,bio],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send("profile saved")
-   }
-  }
- )
-})
-
-app.get("/profile/:username",(req,res)=>{
- const username=req.params.username
- db.query(
-  "SELECT * FROM profiles WHERE username=?",
-  [username],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send(result)
-   }
-  }
- )
-})
-
-app.post("/favourite",(req,res)=>{
- const {username,title,poster,type}=req.body
- db.query(
-  "INSERT INTO favourites (username,title,poster,type) VALUES (?,?,?,?)",
-  [username,title,poster,type],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send("added")
-   }
-  }
- )
 })
 
 app.get("/favourites/:username",(req,res)=>{
- const username=req.params.username
- db.query(
-  "SELECT * FROM favourites WHERE username=?",
-  [username],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send(result)
-   }
-  }
- )
+const u=req.params.username
+db.query("SELECT * FROM favourites WHERE username=?",[u],(err,result)=>{if(err)res.json([]);else res.json(result)})
+})
+
+app.post("/favourite",(req,res)=>{
+const {username,title,poster,type}=req.body
+db.query("INSERT INTO favourites(username,title,poster,type) VALUES(?,?,?,?)",[username,title,poster,type],(err,result)=>{res.send("OK")})
+})
+
+app.post("/profile",(req,res)=>{
+const {username,name,bio}=req.body
+db.query("INSERT INTO profiles(username,name,bio) VALUES(?,?,?) ON DUPLICATE KEY UPDATE name=?,bio=?",[username,name,bio,name,bio],(err,result)=>res.send("OK"))
+})
+
+app.get("/followers/:username",(req,res)=>{
+const u=req.params.username
+db.query("SELECT follower FROM follows WHERE target=?",[u],(err,result)=>{if(err)res.json([]);else res.json(result.map(r=>r.follower))})
+})
+
+app.get("/following/:username",(req,res)=>{
+const u=req.params.username
+db.query("SELECT target FROM follows WHERE follower=?",[u],(err,result)=>{if(err)res.json([]);else res.json(result.map(r=>r.target))})
 })
 
 app.post("/follow",(req,res)=>{
- const {user,follower}=req.body
- db.query(
-  "INSERT INTO followers (user,follower) VALUES (?,?)",
-  [user,follower],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send("followed")
-   }
-  }
- )
+const {follower,target}=req.body
+db.query("INSERT INTO follows(follower,target) VALUES(?,?) ON DUPLICATE KEY UPDATE follower=follower",[follower,target],(err,result)=>res.send("OK"))
 })
 
-app.get("/followers/:user",(req,res)=>{
- const user=req.params.user
- db.query(
-  "SELECT * FROM followers WHERE user=?",
-  [user],
-  (err,result)=>{
-   if(err){
-    res.send(err)
-   }else{
-    res.send(result)
-   }
-  }
- )
+app.get("/recommendations/:username",(req,res)=>{
+const u=req.params.username
+db.query("SELECT DISTINCT f2.username FROM favourites f1 JOIN favourites f2 ON f1.title=f2.title AND f1.username<>f2.username WHERE f1.username=?",[u],(err,result)=>{if(err)res.json([]);else res.json(result.map(r=>r.username))})
 })
 
-app.listen(5000,()=>{
- console.log("Server running on port 5000")
-})
+app.listen(5000,()=>console.log("Server running on port 5000"))
